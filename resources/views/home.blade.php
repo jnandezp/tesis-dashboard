69db060e2eae0ca172a8b2ea52300aa2fa813f5d
Jose Ivan Nandez Parra <jnandezp@users.noreply.github.com>
2022-09-25 12:05:06 -0500

    actualizacion de repositorio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,37 @@
 @extends('layouts.app')
 
+@section('title')
+    Dashboard
+@endsection
+
+@section('breadcrumb')
+    <li class="breadcrumb-item"><a href="#">Home</a></li>
+    <li class="breadcrumb-item active">Blank Page</li>
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <!-- Default box -->
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Dashboard</h3>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+            <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                    <i class="fas fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                    <i class="fas fa-times"></i>
+                </button>
             </div>
         </div>
+        <div class="card-body">
+            Start creating your amazing application!
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer">
+            Footer
+        </div>
+        <!-- /.card-footer-->
     </div>
-</div>
+    <!-- /.card -->
 @endsection

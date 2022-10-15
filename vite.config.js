bb75ae72cbc3cf1b489ea638c99277f57ac71847
Jose Ivan Nandez Parra <jnandezp@gmail.com>
2022-10-15 11:25:34 -0500

    actualizacion repositorio

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
+                'Modules/Blog/Resources/assets/sass/app.scss',
+                'Modules/Blog/Resources/assets/js/app.js',
             ],
             refresh: true,
         }),
